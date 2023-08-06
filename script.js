34e47345b57f950470993a16de6a4f7d1d68ffdf
109062109 <waylon620@gmail.com>
2023-08-06 12:59:53 +0800

    add download and clear msg

--- script.js
+++ script.js
@@ -3,16 +3,19 @@ const msgerInput = get(".msger-input");
 const msgerChat = get(".msger-chat");
 const problem_type = document.getElementById("problem_type");
 
-const history = []; //record the history
+var history = []; //record the history
 
+var full_history = [];
 
-const BOT_MSGS = [
-  "Hi, how are you?",
-  "Ohh... I can't understand what you trying to say. Sorry!",
-  "I like to play games... But I don't know how to play!",
-  "Sorry if my answers are not relevant. :))",
-  "I feel sleepy! :("
-];
+document.getElementById("downloadBtn").addEventListener("click", () => {
+  saveHistoryToJson();
+});
+
+document.getElementById("clearBtn").addEventListener("click", () => {
+  msgerChat.innerHTML = ''; // Clear the chat interface
+  history = []; // Clear the history
+  full_history = [];
+});
 
 // Icons made by Freepik from www.flaticon.com
 const BOT_IMG = "angular.svg";
@@ -26,28 +29,28 @@ msgerForm.addEventListener("submit", event => {
     const msgText = msgerInput.value;
     if (!msgText) return;
 
-    appendMessage(PERSON_NAME, PERSON_IMG, "right", msgText);
-    const response = GPT_api(msgText);
+    var user_time = appendMessage(PERSON_NAME, PERSON_IMG, "right", msgText);
+    const response = GPT_api(msgText, user_time);
     //   botResponse(response);
     msgerInput.value = "";
 
 });
 
-async function GPT_api(message){
+async function GPT_api(message, user_time){
     const type = problem_type.value;
     console.log(type);
     var responseMessage = "";
-    console.log(message);
+    // console.log(message);
     const requestBody = {
         model: 'gpt-3.5-turbo',
         messages: [{ role: 'system', content: 'You are a helpful assistant.' }, ...history, { role: 'user', content: message }]
     };
-    console.log(requestBody.messages);
+    // console.log(requestBody.messages);
     const requestOptions = {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          Authorization: 'Bearer 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'
+          Authorization: 'Bearer '
         },
         body: JSON.stringify(requestBody)
     };
@@ -64,21 +67,23 @@ async function GPT_api(message){
         // Use the response message as needed
         console.log(responseMessage);
         addToHistory(message, responseMessage);
-        
-    } else {
+      } else {
         // Handle the error case
         console.log('Error:', response.statusText);
     }
     } catch (error) {
-    // Handle the error case
-    console.log('Error:', error);
+      // Handle the error case
+      console.log('Error:', error);
     }
-    botResponse(responseMessage);
+    var ai_time = botResponse(responseMessage);
+    addToFull_History(message, user_time, responseMessage, ai_time);
+    console.log(full_history);
     return responseMessage;
 }
 
 function appendMessage(name, img, side, text) {
   //   Simple solution for small apps
+  var time = formatDate(new Date());
   const msgHTML = `
     <div class="msg ${side}-msg">
       <div class="msg-img" style="background-image: url(${img})"></div>
@@ -86,7 +91,7 @@ function appendMessage(name, img, side, text) {
       <div class="msg-bubble">
         <div class="msg-info">
           <div class="msg-info-name">${name}</div>
-          <div class="msg-info-time">${formatDate(new Date())}</div>
+          <div class="msg-info-time">${time}</div>
         </div>
 
         <div class="msg-text"><pre>${text}</pre></div>
@@ -96,17 +101,17 @@ function appendMessage(name, img, side, text) {
 
   msgerChat.insertAdjacentHTML("beforeend", msgHTML);
   msgerChat.scrollTop += 500;
+
+  return time;
 }
 
 function botResponse(response) {
-    const r = random(0, BOT_MSGS.length - 1);
-    const msgText = BOT_MSGS[r];
-    const delay = msgText.split(" ").length * 100;
-    appendMessage(BOT_NAME, BOT_IMG, "left", response);
+    var ai_time = appendMessage(BOT_NAME, BOT_IMG, "left", response);
 
     // setTimeout(() => {
     //     appendMessage(BOT_NAME, BOT_IMG, "left", responseMessage);
     // }, delay);
+    return ai_time;
 }
 
 function addToHistory(input, response) {
@@ -114,16 +119,51 @@ function addToHistory(input, response) {
     history.push({ role: 'assistant', content: response });
 }
 
+function addToFull_History(input, time, response, ai_time) {
+    full_history.push({ role: 'user', content: input, time: time});
+    full_history.push({ role: 'assistant', content: response, time : ai_time });
+}
+
+const data = { user_id: "user_id", type: problem_type.value, history: full_history };
+
+// Function to save history to a JSON file
+function saveHistoryToJson() {
+  data.type = problem_type.value;
+  data.history = full_history;
+  const jsonData = JSON.stringify(data, null, 2);
+  const blob = new Blob([jsonData], { type: "application/json" });
+  const url = URL.createObjectURL(blob);
+  
+  // Create a link element to download the JSON file
+  const downloadLink = document.createElement("a");
+  downloadLink.href = url;
+  downloadLink.download = "chat_history.json";
+  document.body.appendChild(downloadLink);
+  
+  // Click the link to trigger the download
+  downloadLink.click();
+  
+  // Remove the link element
+  document.body.removeChild(downloadLink);
+}
+
+
 // Utils
 function get(selector, root = document) {
   return root.querySelector(selector);
 }
 
 function formatDate(date) {
+  const y = date.getFullYear();
+  const mo = "0" + (date.getMonth() + 1);
+  const d = "0" + date.getDate();
   const h = "0" + date.getHours();
   const m = "0" + date.getMinutes();
 
-  return `${h.slice(-2)}:${m.slice(-2)}`;
+  const formattedDate = `${y}-${mo.slice(-2)}-${d.slice(-2)}`;
+  const formattedTime = `${h.slice(-2)}:${m.slice(-2)}`;
+
+  return `${formattedDate} ${formattedTime}`;
 }
 
 function random(min, max) {
