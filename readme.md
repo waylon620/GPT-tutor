5e6d4bb08e33bd7392b3f6ce9f647225cfed8de3
許淳 <95380177+RexHsu09@users.noreply.github.com>
2023-09-04 16:58:04 +0800

    readme & batch file update

--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
-1. run bing.py
-2. 執行 server.bat
-3. project backend terminal: go run main.go
+1. 加入API_KEY.txt的檔案，內容為openai的api key。
+2. 匯出bingChat的cookies json文件，複製到`bingChatCookies.json`這個檔案中，預設為李緯倫的帳號的cookies。
+3. 執行server.bat即可啟動系統。
+4. 等待mongodb連線成功後，開起瀏覽器的無痕模式並輸入前端網址 e.g. http://127.0.0.1:1111。
--- server.bat
+++ server.bat
@@ -1,3 +1,6 @@
 @echo off
-start http-server -p 1112
-start "C:\Users\user\AppData\Local\Microsoft\WindowsApps\python.exe" "C:\Users\user\Desktop\vscode\yishin_project\chatroom_example\bing.py"
+@echo Starting server...
+start http-server -p 1111
+start python bing.py 
+cd backend 
+start go run main.go
